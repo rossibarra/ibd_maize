e290d9d8d72aba9e61293c01afab26a6d2267095
rossibarra <rossibarra@ucdavis.edu>
2020-04-07 14:48:57 -0700

    scripts changed, first ibdne run with crappy results for JRIAL1

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -10,5 +10,12 @@
 
 VCF=data/JRIAL1.vcf
 
-#vcftools --gzvcf /group/jrigrp10/al/phased_single_pop/LRs_eagle2_hapcut.merged_Chr$SLURM_ARRAY_TASK_ID.vcf.gz 	--max-alleles 2 --min-alleles 2 --phased --minQ 500 --remove-indels  --max-missing .02 --minDP 20	--maxDP 200 --recode --stdout | gzip > LR.allpop.phased.filtered22.$SLURM_ARRAY_TASK_ID.vcf.gz
-vcftools --gzvcf $VCF.gz --max-alleles 2 --min-alleles 2  --remove-indels --minQ 30  --minQ 30 --minDP 15 --maxDP 100 --minGQ 30 --max-missing 0.1 --recode --stdout | gzip > $VCF.filtered.gz
+vcftools --gzvcf $VCF.gz --max-alleles 2 --min-alleles 2  --remove-indels --minQ 30  --minQ 30 --minDP 15 --maxDP 100 --minGQ 30 --max-missing 0.1 --recode --stdout > $VCF.filtered
+
+bgzip -c $VCF.filtered > $VCF.filtered.gz  #compress vcf
+tabix -p vcf $VCF.filtered.gz  # index compressed vcf
+tabix --list-chroms $VCF.filtered.gz > $VCF.chromosomes.txt  # save all the chromosome names into a file
+
+while IFS= read -r line; do
+  tabix $VCF.filtered.gz $line > $VCF.filtered.$line.vcf;
+done < $VCF.chromosomes.txt  # make an individual vcf for each chromosome
--- scripts/merge.sh
+++ scripts/merge.sh
@@ -4,16 +4,17 @@
 #SBATCH -o /home/jri/projects/ibd/logs/merge.out-%j.txt
 #SBATCH -e /home/jri/projects/ibd/logs/merge.error-%j.txt
 #SBATCH -t24:00:00
-#SBATCH -n 1
-#SBATCH --mem 1G 
-#SBATCH --array 1-10
+#SBATCH -n 2
+#SBATCH --mem 10G 
+#SBATCH -p bigmemh
 
-in=results/LJRIAL1.vcf.gz.ibdseq.out.ibd
+basename=JRIAL1.vcf.filtered.gz
+in=results/$basename.ibdseq.out.ibd
 #R.allpop.phased.filtered.imputed.$SLURM_ARRAY_TASK_ID.refined.ibd.gz 
-out=results/LR.allpop.phased.filtered.imputed.$SLURM_ARRAY_TASK_ID.merge 
-vcf=results/LR.allpop.phased.filtered.imputed.$SLURM_ARRAY_TASK_ID.vcf.gz 
+out=results/$basename.merge 
+vcf=data/$basename 
 gap=0.6 
 discord=1 
 map=data/ogut.map  
 
-zcat  $in  | java -Xmx8000m  -jar /home/jri/src/ibd/merge-ibd-segments.17Jan20.102.jar $vcf   $map $gap $discord  > $out
+cat  $in  | java -Xmx8000m  -jar /home/jri/src/ibd/merge-ibd-segments.17Jan20.102.jar $vcf   $map $gap $discord  > $out
