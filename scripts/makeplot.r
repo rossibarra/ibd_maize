78c85cb7c4a4867b3a5f64a1bdc973eed4a78dba
rossibarra <rossibarra@ucdavis.edu>
2020-04-11 12:05:35 -0700

    cat power

--- scripts/makeplot.r
+++ scripts/makeplot.r
@@ -1,4 +1,6 @@
 library(tidyverse)
+
+system("mv ~/projects/ibd/results/JRIAL1/neplot.pdf ~/projects/ibd/results/JRIAL1/old_neplot.pdf")
 neplot<-read.table("~/projects/ibd/results/JRIAL1/JRIAL1_ne.ne",header=T) %>%  
   ggplot(aes(x=GEN)) +
   geom_ribbon(aes(ymin=log10(LWR.95.CI),ymax=log10(UPR.95.CI)),fill = "grey70") +
